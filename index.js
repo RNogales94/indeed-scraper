4d44583114e2d45cc365ce1c84e4dc582f110cc2
RNogales <rnogales@correo.ugr.es>
2018-04-28 12:54:19 +0200

    Procfile

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const indeed = require('./lib/scraper.js')
+
+const queryOptions = {
+  query: '',
+  city: 'Granada, España',
+  radius: '25',
+  level: '',
+  jobType: 'fulltime',
+  maxAge: '7',
+  sort: 'date',
+  limit: '10'
+};
+
+var JobList
+
+indeed.query(queryOptions).then(res => {
+    JobList = res; // An array of Job objects
+});
+
+const PORT = process.env.PORT || 5000
+
+express()
+  .get('/', (req, res) => res.send("Hola Martita"))
+  .get('/jobs', (req, res) => res.send(./GR_jobs.js))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
